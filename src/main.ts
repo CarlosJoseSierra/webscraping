209676370e19e93eb0eabf7db32b1cb350552216
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2026-07-29 18:01:57 -0500

    first commit webs

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,12 @@ async function bootstrap() {
   // Habilitar CORS
   app.enableCors({
     //origin: '*', // En producción, cámbialo por tu dominio específico (ej: 'https://tuapp.com')
-    origin: 'http://localhost:4200',
+    origin: 'https://audictexfor.online',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
